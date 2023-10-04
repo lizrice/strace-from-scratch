c344b9715fe0b4cee73d39bc434c42dcd9db0e9f
Richqrd <richard.tingstad@gmail.com>
2023-10-04 23:00:51 +0200

    print OPEN filename & FD

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 )
 
 func main() {
-	var regs syscall.PtraceRegs
 	var ss syscallCounter
 
 	ss = ss.init()
@@ -24,28 +23,48 @@ func main() {
 	}
 
 	cmd.Start()
-	err := cmd.Wait()
+	err := cmd.Wait() // cmd is paused
 	if err != nil {
 		fmt.Printf("Wait returned: %v\n", err)
 	}
 
+	var regs syscall.PtraceRegs
 	pid := cmd.Process.Pid
 	exit := true
 
 	for {
-		if exit {
-			err = syscall.PtraceGetRegs(pid, &regs)
-			if err != nil {
+		if err = syscall.PtraceGetRegs(pid, &regs); err != nil {
+			if err.Error() == "no such process" {
 				break
 			}
+			panic(err)
+		}
+		if exit {
 
-			// Uncomment to show each syscall as it's called
+			// https://man7.org/linux/man-pages/man2/syscall.2.html
+			//   Arch/ABI    arg1  arg2  arg3  arg4  arg5  arg6  arg7   Notes
+			//   ────────────────────────────────────────────────────────────
+			//   x86-64      rdi   rsi   rdx   r10   r8    r9    -
+			//
+			//   Arch/ABI    Instruction       System  Ret  Ret  Error  Notes
+			//                                 call #  val  val2
+			//   ────────────────────────────────────────────────────────────
+			//   x86-64      syscall           rax     rax  rdx  -      5
+
+			f := ""
+			if regs.Orig_rax == syscall.SYS_OPEN {
+				fmt.Printf("%d %x\n", regs.Rdi, regs.Rdi)
+				path := readPtraceText(pid, uintptr(regs.Rdi))
+				fd := int(regs.Rax)
+				f = fmt.Sprintf(`("%s") => %d`, path, fd)
+			}
+
 			name := ss.getName(regs.Orig_rax)
-			fmt.Printf("%s\n", name)
+			fmt.Printf("%s %s\n", name, f)
 			ss.inc(regs.Orig_rax)
 		}
 
-		err = syscall.PtraceSyscall(pid, 0)
+		err = syscall.PtraceSyscall(pid, 0) // wait for next syscall to begin or exit
 		if err != nil {
 			panic(err)
 		}
@@ -59,4 +78,18 @@ func main() {
 	}
 
 	ss.print()
+}
+
+func readPtraceText(pid int, addr uintptr) string {
+	s := ""
+	buf := []byte{1}
+	for i := addr; buf[0] != byte(0); i++ {
+		if c, err := syscall.PtracePeekText(pid, i, buf); err != nil {
+			panic(err)
+		} else if c == 0 {
+			break
+		}
+		s += string(buf)
+	}
+	return s
 }
